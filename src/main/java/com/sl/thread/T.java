0f396e4f1116769b2e204b73d23211e70fc82143
kayang0000 <18410422493@163.com>
2019-01-02 13:48:11 +0800

    submit others

--- src/main/java/com/sl/thread/T.java
+++ src/main/java/com/sl/thread/T.java
@@ -1,15 +1,23 @@
 package com.sl.thread;
 
-public class {
-	public static void main(String[] args) {
-		m1();
-	}
-	
-	public static  void m1() {
-		m2();
-		m3();
-	}
-	
-	public static void m2() {}
-	public static void m3() {}
+/**
+ * @author kqyang
+ * @company sl
+ * @date 2018/12/28 20:05
+ */
+public class T {
+    public static void main(String[] args) {
+        m1();
+    }
+
+    public static void m1() {
+        m2();
+        m3();
+    }
+
+    public static void m2() {
+    }
+
+    public static void m3() {
+    }
 }
