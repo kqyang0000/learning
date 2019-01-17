3f7622f6985bbf6dbe4fc74ab309e6df6089362f
kayang0000 <18410422493@163.com>
2019-01-17 22:40:38 +0800

    about sleep,join,yield of Thread

--- src/main/java/com/sl/thread/TestThread2.java
+++ src/main/java/com/sl/thread/TestThread2.java
@@ -1,19 +1,26 @@
 package com.sl.thread;
 
+/**
+ * <p>同一个Runner线程对象可以同时起两个线程</p>
+ *
+ * @author kqyang
+ * @version 1.0
+ * @date 2019/1/17 22:28
+ */
 public class TestThread2 {
-	public static void main(String args[]) {
-		Runner2 r = new Runner2();
-		Thread t1 = new Thread(r);
-		Thread t2 = new Thread(r);
-		t1.start();
-		t2.start();
-	}
+    public static void main(String args[]) {
+        Runner2 r = new Runner2();
+        Thread t1 = new Thread(r);
+        Thread t2 = new Thread(r);
+        t1.start();
+        t2.start();
+    }
 }
 
 class Runner2 implements Runnable {
-	public void run() {
-		for(int i=0; i<30; i++) {	
-			System.out.println("No. " + i);
-		}
-	}
+    public void run() {
+        for (int i = 0; i < 30; i++) {
+            System.out.println("No. " + i);
+        }
+    }
 }
